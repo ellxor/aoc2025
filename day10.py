9e72120a58e7aad390d67dc4dfd221af9851e0cb
ellxor <ellxor@protonmail.ch>
2025-12-14 14:20:14 +0000

    Day 10 :: drop z3 dependency and use divide-and-conquer approach to find coefficients

--- day10.py
+++ day10.py
@@ -1,60 +1,66 @@
-from functools import reduce
-from itertools import chain, combinations
+from itertools import combinations, product
 from sys import stdin
-
-import z3
 
 
 def parse(line):
     tokens = [token[1:-1] for token in line.split()]
     indicator, wiring, joltage = tokens[0], tokens[1:-1], tokens[-1]
 
-    indicator = sum([(c == "#") << i for i, c in enumerate(indicator)])
-    wiring = [sum([1 << int(n) for n in token.split(",")]) for token in wiring]
-    joltage = list(map(int, joltage.split(",")))
+    indicator = tuple((c == "#") for c in indicator)
+    wiring = [tuple(map(int, token.split(","))) for token in wiring]
+    joltage = tuple(map(int, joltage.split(",")))
 
+    wiring = [tuple(int(i in w) for i, _ in enumerate(indicator)) for w in wiring]
     return indicator, wiring, joltage
 
 
-def powerset(s):
-    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+def build_parity_table(wiring, count):
+    table = {parity: [] for parity in product(range(2), repeat=count)}
+    zeros = tuple(0 for _ in range(count))
+
+    for length in range(len(wiring) + 1):
+        for subset in combinations(wiring, length):
+            jolt = tuple(sum(c) for c in zip(zeros, *subset))
+            parity = tuple(j % 2 for j in jolt)
+            table[parity].append((len(subset), jolt))
+
+    return table
 
 
-def solve_part1(puzzle):
-    (indicator, wiring, _) = puzzle
+def solve_part2(table, joltage, seen):
+    if all(j == 0 for j in joltage):
+        return 0
 
-    for subset in powerset(wiring):
-        if indicator == reduce(lambda a, b: a ^ b, subset, 0):
-            return len(subset)
+    if joltage in seen:
+        return seen[joltage]
 
-    assert False, "failed to solve puzzle"
+    parity = tuple(j % 2 for j in joltage)
+    presses = 10**10
 
+    for length, diff in table[parity]:
+        if all(d <= j for d, j in zip(diff, joltage)):
+            divide = solve_part2(
+                table, tuple((j - d) // 2 for d, j in zip(diff, joltage)), seen
+            )
 
-def solve_part2(puzzle):
-    (_, wiring, joltage) = puzzle
-    vectors = [[(w >> bit) & 1 for bit in range(len(joltage))] for w in wiring]
-    coeffs = [z3.Int(f"c_{i}") for i, _ in enumerate(vectors)]
+            presses = min(
+                presses,
+                length + 2 * divide,
+            )
 
-    solver = z3.Optimize()
-    solver.minimize(z3.Sum(coeffs))
-
-    for ci in coeffs:
-        solver.add(ci >= 0)
-
-    for i, j in enumerate(joltage):
-        solver.add(j == z3.Sum(c for c, v in zip(coeffs, vectors) if v[i]))
-
-    assert solver.check()
-    model = solver.model()
-
-    return sum([model[c].as_long() for c in coeffs])
+    seen[joltage] = presses
+    return presses
 
 
 def solve(input):
-    puzzles = [parse(line) for line in input]
+    part1, part2 = 0, 0
 
-    part1 = sum(map(solve_part1, puzzles))
-    part2 = sum(map(solve_part2, puzzles))
+    for puzzle in map(parse, input):
+        indicator, wiring, joltage = puzzle
+        table = build_parity_table(wiring, len(joltage))
+
+        part1 += table[indicator][0][0]
+        part2 += solve_part2(table, joltage, {})
 
     return part1, part2
 
